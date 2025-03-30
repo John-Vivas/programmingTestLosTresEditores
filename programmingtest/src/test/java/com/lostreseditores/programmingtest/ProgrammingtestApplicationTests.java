755f09e92d3239b9f2e9fe3731476b00f5c03d12
unknown <j.vivas0122@gmail.com>
2025-03-29 23:17:54 -0500

    first commit

---
+++ programmingtest/src/test/java/com/lostreseditores/programmingtest/ProgrammingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostreseditores.programmingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
